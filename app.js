492af49eff52bf6438f47d204849c15354035c0a
ziyad188 <mohdziyadk@gmail.com>
2022-10-31 20:21:23 +0530

    added

--- app.js
+++ app.js
@@ -10,9 +10,26 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 //database
-mongoose.connect("mongodb+srv://ziyad:ziyad@cluster0.qzthgrr.mongodb.net/?retryWrites=true&w=majority/studentsDB",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://ziyad:ziyad@cluster0.qzthgrr.mongodb.net/studentsDB?retryWrites=true&w=majority",{useNewUrlParser: true});
 //routes
 //collections
+//admin portal
+const adminSchema ={
+    username: String,
+    password: String
+};
+const Admin = mongoose.model("Admin",adminSchema);
+//food hostler bill
+const hostlerSchema = {
+    id: String,
+    name:String,
+    date:String,
+    breakfast: String ,
+    lunch: String,
+    dinner: String,
+    amount: String
+}
+const Bill = mongoose.model("Bill",hostlerSchema);
 const studentSchema = {
     id: String,
     name: String,
@@ -34,35 +51,109 @@ app.get("/students/:id", function(req,res){
         var idreq = req.params['id'];
         if(!err){
             console.log(foundList);
-            if(foundList.batch === "host"){
+            if(foundList.batch === "hostler"){
                 
                 var datetime = new Date();
+                console.log(datetime.getDay());
+                var day = datetime.getDate();
+                var month = datetime.getMonth();
+                var year = datetime.getFullYear();
+                var date = day+"/"+month+"/"+year;
                 var hour = datetime.getHours();
+                
                 if((hour>6 && hour<9) && breakfast == 0){
-                   res.render("auth",{text:"Authenticated"});
                     breakfast=1;
+                  Bill.findOne({id:idreq} && {date:date},function(err,foundlist){
+                    if(foundlist == null){
+                        const bill = new Bill({
+                            id:idreq,
+                            name:foundList.name,
+                            date:date,
+                            breakfast:"yes",
+                            amount:135
+                        })
+                        bill.save(function(err){
+                            if(!err){
+                                res.render("auth",{text:"Authenticated"});
+                            }
+                        });
+                    }else{
+                        Bill.findOneAndUpdate({id: idreq},{breakfast:"yes"},function(err,result){
+                            if(!err){
+                                res.render("auth",{text:"Authenticated"});
+                            }
+                        }) 
+                    }
+                  })
+                    
 
                 }else if((hour>9 && hour<15) && lunch == 0){
-                    res.render("auth",{text:"Authenticated"});
                     lunch=1;
+                    Bill.findOne({id:idreq} && {date:date},function(err,foundlist){
+                        if(foundlist == null){
+                            const bill = new Bill({
+                                id:idreq,
+                                name:foundList.name,
+                                date:date,
+                                lunch:"yes",
+                                amount:135
+                            })
+                            bill.save(function(err){
+                                if(!err){
+                                    res.render("auth",{text:"Authenticated"});
+                                }
+                            });
+                        }else{
+                            Bill.findOneAndUpdate({id: idreq} && {date:date},{lunch:"yes"},function(err,result){
+                                if(!err){
+                                    res.render("auth",{text:"Authenticated"});
+                                }
+                            }) 
+                        }
+                      })
+                        
 
                 }else if((hour>15 && hour<19) && snacks == 0){
                     res.render("auth",{text:"Authenticated"});
                     snacks = 1;
+                   
 
-                }else if ((hour>19 && hour<6) && dinnner ==0){
-                    res.render("auth",{text:"Authenticated"});
+                }else if ((hour>19 ) && dinnner ==0){
+                    
                     dinnner = 1;
-
+                    Bill.findOne({id:idreq},function(err,foundlist){
+                        if(foundlist == null){
+                            const bill = new Bill({
+                                id:idreq,
+                                name:foundList.name,
+                                date:date,
+                                dinner:"yes",
+                                amount:135
+                            })
+                            bill.save(function(err){
+                                if(!err){
+                                    res.render("auth",{text:"Authenticated"});
+                                }
+                            });
+                        }else{
+                            Bill.findOneAndUpdate({id: idreq},{dinner:"yes"},function(err,result){
+                                if(!err){
+                                    res.render("auth",{text:"Authenticated"});
+                                }
+                            }) 
+                        }
+                      })
+                        
                 }else{
                     res.render("auth",{text:"You already purchased your food"});
                 }
+
                 
             }else{
                     if(foundList.meals>0){
                     var meal = foundList.meals-1;
                     Student.findOneAndUpdate({id:idreq},{meals:meal},function(err,result){
-                        if(err){
+                        if(!err){
                            res.render("auth",{text:"Authenticated"});
                         }
                     });
