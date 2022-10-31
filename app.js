7fa6d62c348fcee4f1504a3638f172ad9cbceb9a
ziyad188 <mohdziyadk@gmail.com>
2022-10-31 14:27:07 +0530

    added files

--- app.js
+++ app.js
@@ -12,11 +12,40 @@ app.set('view engine', 'ejs');
 //database
 mongoose.connect("mongodb+srv://ziyad:ziyad@cluster0.qzthgrr.mongodb.net/?retryWrites=true&w=majority/studentsDB",{useNewUrlParser: true});
 //routes
+//collections
+const studentSchema = {
+    id: String,
+    name: String,
+    batch: String,
+    meals: Number,
+
+  };
+  
+  const Student = mongoose.model("Student", studentSchema);
+
 app.get("/students/:id", function(req,res){
     console.log(req.params['id']);
     Student.findOne({id: req.params['id']}, function(err, foundList){
         if(!err){
             console.log(foundList);
+            if(foundList.batch === "host"){
+                res.send("authenticated");
+            }else{
+                Student.findOne({id: req.params['id']},function(err, found){
+                    if(found.meals>0){
+                    var meal = found.meals-1;
+                    Student.updateOne({meals:meal},function(err,result){
+                        if(!err){
+                           res.send("authenticated");
+                        }
+                    });
+                }else{
+                    res.send("please recharge your nfc tag");
+                }
+
+
+                })
+            }
         }else{
             console.log(err);
         }
@@ -25,15 +54,7 @@ app.get("/students/:id", function(req,res){
 });
 app.get("/", function(req,res){
     res.render("index");
-    const studentSchema = {
-        id: String,
-        name: String,
-        batch: String,
-        meals: String,
-
-      };
-      
-      const Student = mongoose.model("Student", studentSchema);
+    
       const orderSchema={
         id:String,
         product: String,
